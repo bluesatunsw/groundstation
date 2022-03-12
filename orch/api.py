1546856b87c013eaf1843b4b5102998c540fd330
Uskompuf <22492406+Uskompuf@users.noreply.github.com>
2022-03-12 18:24:42 +1100

    Fix formatting

--- orch/api.py
+++ orch/api.py
@@ -1,25 +1,41 @@
-from flask import Flask, render_template
+"""
+Functions to interface with the N2YO API.
+"""
 import requests
-def get_satellite():
-    return
-
-def get_visual_passes():
-    return
-
-def send_reqs(i_wanna_read):
-    #req = print(f'https://api.n2yo.com/rest/v1/satellite/{i_wanna_read}')
-    try:
-        req_url = f'https://api.n2yo.com/rest/v1/satellite/{i_wanna_read}/&apiKey=89SYPR-D26PHV-PZFC8D-4Q3K'
-        print(req_url)
-        return requests.get(req_url).json()
-    except:
-        print("no request came back :[")
-
-def get_whats_up(observer_lat, observer_lng, observer_alt, search_radius, category_id):
-    i_wanna_read = f'above/{observer_lat}/{observer_lng}/{observer_alt}/{search_radius}/{category_id}'
-    return send_reqs(i_wanna_read)
 
 
-def get_radiopasses(id, observer_lat, observer_lng, observer_alt, days, min_elevation = 15):
-    i_wanna_read = f'radiopasses/{id}/{observer_lat}/{observer_lng}/{observer_alt}/{days}/{min_elevation}'
-    return send_reqs(i_wanna_read)
+def send_reqs(arguments):
+    """
+    Return result of request to N2YO API using provided arguments.
+    """
+    api_key = "89SYPR-D26PHV-PZFC8D-4Q3K"
+    req_url = f'https://api.n2yo.com/rest/v1/satellite/{arguments}/&apiKey={api_key}'
+    req_res = requests.get(req_url)
+    return req_res.json()
+
+
+def get_whats_up(
+        observer_lat=33.8688,
+        observer_lng=151.2093,
+        observer_alt=3,
+        search_radius=75,
+        category_id=0):
+    """
+    Set paramters for N2YO What's up? API or use reasonable defaults.
+    """
+    arguments = f'above/{observer_lat}/{observer_lng}/{observer_alt}/{search_radius}/{category_id}'
+    return send_reqs(arguments)
+
+
+def get_radiopasses(
+        norad_id=25544,
+        observer_lat=33.8688,
+        observer_lng=151.2093,
+        observer_alt=3,
+        days=7,
+        min_elevation=15):
+    """
+    Set paramters for N2YO Get radio passes API or use reasonable defaults.
+    """
+    arguments = f'radiopasses/{norad_id}/{observer_lat}/{observer_lng}/{observer_alt}/{days}/{min_elevation}'
+    return send_reqs(arguments)
--- orch/serve.py
+++ orch/serve.py
@@ -1,46 +1,50 @@
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
+"""
+The backend server used by all modules to communicate.
+"""
 from json import dumps
-#.\Scripts\activate   
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import api as apis
 
 app = Flask(__name__)
 CORS(app)
 
-all_req_string = {'whats_up': ['observer_lat', 'observer_lng', 'observer_alt', 'search_radius', 'category_id'],
-'get_radiopasses:': ['id', 'observer_lat', 'observer_lng', 'observer_alt', 'days', 'min_elevation']}
 
 @app.route('/whats_up')
 def whats_up_request():
-    try:
-        lat_req = request.args.get('observer_lat')
-        long_req = request.args.get('observer_lng')
-        search_req = request.args.get('search_radius')
-        alt_req = request.args.get('observer_alt')
-        catid_req = request.args.get('category_id')
-        api_input = apis.get_whats_up(lat_req, long_req, alt_req, search_req, catid_req)
-        return jsonify(api_input)
-    except Exception as e:
-        error = f"oops! {e}"
-        return jsonify(error)
+    """
+    An internal endpoint that resolves to the N2YO What's up? API.
+    """
+    lat_req = request.args.get('observer_lat')
+    long_req = request.args.get('observer_lng')
+    search_req = request.args.get('search_radius')
+    alt_req = request.args.get('observer_alt')
+    catid_req = request.args.get('category_id')
+    api_input = apis.get_whats_up(
+        lat_req, long_req, alt_req, search_req, catid_req)
+    return jsonify(api_input)
+
 
 @app.route('/radiopasses')
 def radiopass_request():
-    try:
-        id_req = request.args.get('id')
-        lat_req = request.args.get('observer_lat')
-        long_req = request.args.get('observer_lng')
-        alt_req = request.args.get('observer_alt')
-        days_req = request.args.get('days')
-        m_ele_req = request.args.get('min_elevation')
-        api_input = apis.get_radiopasses(id_req, lat_req, long_req, alt_req, days_req, m_ele_req)
-        return jsonify(api_input)
-    except Exception as e:
-        error = f"oops! {e}"
-        return jsonify(error)
+    """
+    An internal endpoint that resolves to the N2YO Get radio passes API.
+    """
+    id_req = request.args.get('id')
+    lat_req = request.args.get('observer_lat')
+    long_req = request.args.get('observer_lng')
+    alt_req = request.args.get('observer_alt')
+    days_req = request.args.get('days')
+    m_ele_req = request.args.get('min_elevation')
+    api_input = apis.get_radiopasses(
+        id_req, lat_req, long_req, alt_req, days_req, m_ele_req)
+    return jsonify(api_input)
 
 
-def defaultHandler(err):
+def default_handler(err):
+    """
+    The error handler for Flask exceptions.
+    """
     response = err.get_response()
     response.data = dumps({
         "code": err.code,
@@ -52,26 +56,8 @@ def defaultHandler(err):
 
 
 app.config['TRAP_HTTP_EXCEPTIONS'] = True
-app.register_error_handler(Exception, defaultHandler)
+app.register_error_handler(Exception, default_handler)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4999)
-
-'''
-@app.route('/testin/<string:name>')
-def t1(name):
-    return render_template("index.html")
-#from distutils.log import debug
-@app.route('/')
-def index():
-    lat_req = request.args.get('latitude')
-    long_req = request.args.get('longitude')
-    api_input = apis.whats_up(1,long_req, lat_req ,4,5)
-    print(lat_req)
-    return jsonify(api_input)
-
-@app.route('/testin/<string:name>')
-def t1(name):
-    return render_template("index.html")
-
-#'''
