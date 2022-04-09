d93126db2ed84b1c0b10ab356c5f0eb1149df106
LukePrior <lukeprior3@gmail.com>
2022-04-09 12:34:21 +1000

    visual passes

--- frontend/static/js/index.ts
+++ frontend/static/js/index.ts
@@ -1,4 +1,4 @@
-import { n2yo_get_radio_passes, gps_position } from './custom_types'
+import { n2yo_get_radio_passes, n2yo_get_visual_passes, gps_position } from './custom_types'
 
 $(document).on('submit', 'form', function (e) {
   get_radio_passes(parseInt($(this).serializeArray()[0].value))
@@ -14,6 +14,18 @@ function get_radio_passes (norad_id: number) {
     type: 'GET',
     dataType: 'json',
     success: function (res: n2yo_get_radio_passes) {
+      update_data_field(res)
+    }
+  })
+}
+
+function get_visual_passes (norad_id: number) {
+  $.ajax({
+    url: 'http://localhost:4999/visualpasses',
+    data: { norad_id: norad_id },
+    type: 'GET',
+    dataType: 'json',
+    success: function (res: n2yo_get_visual_passes) {
       update_data_field(res)
     }
   })
