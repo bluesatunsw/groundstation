8c1e1cd3ba3961393f2893bff9787e3f6bcaae08
Matt Rossouw <>
2022-05-09 19:23:00 +1000

    Refactored secrets to be safe

--- orch/api.py
+++ orch/api.py
@@ -2,13 +2,14 @@
 Functions to interface with the N2YO API.
 """
 import requests
+import json
 
+api_key = json.load(open('../secrets.json'))['n2yo']
 
 def send_reqs(arguments):
     """
     Return result of request to N2YO API using provided arguments.
     """
-    api_key = "89SYPR-D26PHV-PZFC8D-4Q3K"
     req_url = f'https://api.n2yo.com/rest/v1/satellite/{arguments}/&apiKey={api_key}'
     try:
         req_res = requests.get(req_url).json()
