5261926ca55609b879cd1e4a66d59186d10ef1f0
Matt Rossouw <>
2022-05-18 17:31:25 +1000

    Further reduction to time functions

--- frontend/src/logic/utility.ts
+++ frontend/src/logic/utility.ts
@@ -3,10 +3,6 @@
 // Convert a time in seconds to a string in the format HH:MM:SS
 function UTCtoD(utc: number): string {
     let n : Date = new Date(utc*1000);
-    let h : string = n.getUTCHours().toString(); 
-    let m : string = n.getUTCMinutes().toString();
-    let s : string = n.getUTCSeconds().toString();
-    
     return n.toLocaleTimeString();
 }
 
